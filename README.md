ccbad994530da57c9e1ee408be791894af90e5a0
Hytalo Antonio <48hytalo@gmail.com>
2022-04-11 13:45:18 -0300

    Bot configurado com venom

---
+++ README.md
@@ -0,0 +1,14 @@
+# TrashBot (Em desenvolvimento)
+
+### O bot que irá te avisar quando tem coleta seletiva ou convencional
+
+#### informações úteis abaixo
+
+### Endpoind busca enderecos
+http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?SingleLine=72240-606&f=json&outSR=%7B%22wkid%22%3A102100%7D&outFields=*wkid%22%3A102100%7D%7D
+
+### Endpoins coleta convencional
+https://siggis.slu.df.gov.br/arcgis/rest/services/Coletas/Coleta_Convencional/FeatureServer/1/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=%7B%22x%22%3A-5357414.687033424%2C%22y%22%3A-1783415.049043646%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%7D%7D&geometryType=esriGeometryPoint&inSR=102100&outFields=*&outSR=102100
+
+### Endpoind coleta seletiva
+https://siggis.slu.df.gov.br/arcgis/rest/services/Coletas/Coleta_Seletiva/FeatureServer/1/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=%7B%22x%22%3A-5358054.221989251%2C%22y%22%3A-1782967.8718537304%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%7D%7D&geometryType=esriGeometryPoint&inSR=102100&outFields=*&outSR=102100
--- src/index.js
+++ src/index.js
@@ -1,72 +1,27 @@
 const venom = require('venom-bot');
 const axios = require('axios');
 
-const urlBuscaEnderecoColetaConvencional = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?SingleLine=';
-const urlBuscaEnderecoColetaSeletiva = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?SingleLine='
+const urlBuscaEnderecoColeta = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?SingleLine=';
+// const urlBuscaEnderecoColetaSeletiva = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?SingleLine='
 
 const urlBuscaColetas = 'http://siggis.slu.df.gov.br/arcgis/rest/services/Coletas/'
 
 
-// venom.create({
-//   session: 'session-name', //name of session
-//   multidevice: false // for version not multidevice use false.(default: true)
-// })
-// .then((client) => start(client))
-// .catch((erro) => {
-//   console.log(erro);
-// });
+venom.create({
+  session: 'session-name', //name of session
+  multidevice: false // for version not multidevice use false.(default: true)
+})
+.then((client) => start(client))
+.catch((erro) => {
+  console.log(erro);
+});
 
-// URL que busca o endereco
-/* 
-  https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?
-  SingleLine=72241-606 ===> valor inserido pelo usuario
-  &f=json ===> campo estatico
-  &outSR={"wkid":102100} ===> campo estatico em observacao
-  &outFields=* ===> campo estativo
-  &distance=50000 ===> campo opcional
-  &location={"x":-5358732.826157182,"y":-1782885.3379671336,"spatialReference":{"wkid":102100,"latestWkid":3857}} ===> campo opcional
-  &maxLocations=6 ===> campo opcional
-*/
-
-// URL que busca os dados da coleta
-/* 
-  https://siggis.slu.df.gov.br/arcgis/rest/services/Coletas/Coleta_Convencional/FeatureServer/1/query?
-  f=json
-  &returnGeometry=true
-  &spatialRel=esriSpatialRelIntersects
-  &geometry={"x":-5358054.221989251,"y":-1782967.8718537304,"spatialReference":{"wkid":102100}}
-  &geometryType=esriGeometryPoint
-  &inSR=102100
-  &outFields=*
-  &outSR=102100
-*/
-
-/*
-  https://siggis.slu.df.gov.br/arcgis/rest/services/Coletas/Coleta_Seletiva/FeatureServer/1/query?
-  f=json
-  &returnGeometry=true
-  &spatialRel=esriSpatialRelIntersects
-  &geometry={"x":-5358054.221989251,"y":-1782967.8718537304,"spatialReference":{"wkid":102100}}
-  &geometryType=esriGeometryPoint
-  &inSR=102100
-  &outFields=*
-  &outSR=102100
-*/
-
-// Endpoind busca enderecos
-// http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?SingleLine=72240-606&f=json&outSR=%7B%22wkid%22%3A102100%7D&outFields=*wkid%22%3A102100%7D%7D
-
-// Endpoins coleta convencional
-// https://siggis.slu.df.gov.br/arcgis/rest/services/Coletas/Coleta_Convencional/FeatureServer/1/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=%7B%22x%22%3A-5357414.687033424%2C%22y%22%3A-1783415.049043646%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%7D%7D&geometryType=esriGeometryPoint&inSR=102100&outFields=*&outSR=102100
-
-// Endpoind coleta seletiva
-// https://siggis.slu.df.gov.br/arcgis/rest/services/Coletas/Coleta_Seletiva/FeatureServer/1/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=%7B%22x%22%3A-5358054.221989251%2C%22y%22%3A-1782967.8718537304%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%7D%7D&geometryType=esriGeometryPoint&inSR=102100&outFields=*&outSR=102100
 
 async function buscaEnderecoColetaConvencional(cep) {
   try {
     const primeiroWkid = encodeURIComponent('{"wkid":102100}');
     const segundoWkid = encodeURIComponent('":102100}}');
-    const urlMontada = urlBuscaEnderecoColetaConvencional+cep+'&f=json&outSR='+primeiroWkid+'&outFields=*wkid'+segundoWkid;
+    const urlMontada = urlBuscaEnderecoColeta+cep+'&f=json&outSR='+primeiroWkid+'&outFields=*wkid'+segundoWkid;
     const response = await axios.get(urlMontada);
     return response;
   } catch (error) {
@@ -94,7 +49,7 @@ async function coletaConvencional(x, y) {
 async function buscaEnderecoColetaSeletiva(cep) {
   try {
     const primeiroWkid = encodeURIComponent('{"wkid":102100}');
-    const urlMontada = urlBuscaEnderecoColetaConvencional+cep+'&f=json&outSR='+primeiroWkid+'&outFields=*&maxLocations=';
+    const urlMontada = urlBuscaEnderecoColeta+cep+'&f=json&outSR='+primeiroWkid+'&outFields=*&maxLocations=';
     const response = await axios.get(urlMontada);
     return response;
   } catch (error) {
@@ -150,52 +105,67 @@ buscaEnderecoColetaSeletiva('72241606').then((x) => {
   });
 });
 
-// function start(client) {
-//   client.onMessage((message) => {
+function start(client) {
+  client.onMessage((message) => {
 
-//     if (message.body === 'Hi' && message.isGroupMsg === false) {
-//       client
-//         .sendText(message.from, 'Welcome Venom 🕷')
-//         .then((result) => {
-//           console.log('Result: ', result); //return object success
-//         })
-//         .catch((erro) => {
-//           console.error('Error when sending: ', erro); //return object error
-//         });
-//     }
+    if ((message.body === 'Oi' || 
+      message.body === 'Olá' || 
+      message.body === 'Bom dia' || 
+      message.body === 'Boa tarde' || 
+      message.body === 'Boa noite') && 
+      message.isGroupMsg === false) {
+      client
+        .sendText(message.from, 'Olá. Eu me chamo TrashBot 🤖.\nEstou aqui para te avisar sobre as coletar diárias de lixo.\n\nTodos os dias irei informar qual tipo de coleta do próximo dia para assim você separar seu lixo de forma correta.\n\nVamos cuidar do meio ambiente juntos.')
+        .then((result) => {
+          console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+    }
 
-//     if (message.body === 'Coleta' && message.isGroupMsg === false) {
+    if (message.body === 'Coleta' && message.isGroupMsg === false) {
 
-//       coletaSeletiva().then((res) => {
-//         const data = JSON.parse(res);
-//         const isFrequencia = String(data.frequencia).search(amanhaSplitStr.replace(/^\w/, (c) => c.toUpperCase()).split(",",1));
-//         if(isFrequencia != -1) {
-//           client.sendText(message.from, 
-//             'Hoje terá coleta seletiva.\nDas ' + data.horario_inicio + ' às ' + data.horario_termino)
-//           .then((result) => {
-//             console.log('Result: ', result); //return object success
-//           })
-//           .catch((erro) => {
-//             console.error('Error when sending: ', erro); //return object error
-//           });
-//         }
-//       });
-      
-//       coletaConvencional().then((res) => {
-//         const data = JSON.parse(res);
-//         const isFrequencia = String(data.frequencia).search(amanhaSplitStr.replace(/^\w/, (c) => c.toUpperCase()).split(",",1));
-//         if(isFrequencia != -1) {
-//           client.sendText(message.from, 
-//             'Hoje terá coleta de organicos.\nDas ' + data.horario_inicio + ' às ' + data.horario_termino)
-//           .then((result) => {
-//             console.log('Result: ', result); //return object success
-//           })
-//           .catch((erro) => {
-//             console.error('Error when sending: ', erro); //return object error
-//           });
-//         }
-//       });
+      buscaEnderecoColetaConvencional('72241606').then((x) => {
+        coletaConvencional(x['data']['candidates'][0]['location']['x'],x['data']['candidates'][0]['location']['y']).then((res) => {
+          const data = JSON.parse(res);
+          const isFrequencia = String(data.frequencia).search(amanhaSplitStr.replace(/^\w/, (c) => c.toUpperCase()).split(",",1));
+          if(isFrequencia != -1) {
+            console.log('Amanhã terá coleta CONVENCIONAL. Das ' + data.horario_inicio + ' às ' + data.horario_termino);
+            client
+            .sendText(message.from, 'Amanhã terá coleta CONVENCIONAL. Das ' + data.horario_inicio + ' às ' + data.horario_termino)
+            .then((result) => {
+              console.log('Result: ', result); //return object success
+            })
+            .catch((erro) => {
+              console.error('Error when sending: ', erro); //return object error
+            });
+          } else {
+            console.log('Amanhã não tem coleta CONVENCIONAL');
+          }
+        });
+      });
 
-//     }
-//   });
-// }
+      buscaEnderecoColetaSeletiva('72241606').then((x) => {
+        coletaSeletiva(x['data']['candidates'][0]['location']['x'],x['data']['candidates'][0]['location']['y']).then((res) => {
+          const data = JSON.parse(res);
+          const isFrequencia = String(data.frequencia).search(amanhaSplitStr.replace(/^\w/, (c) => c.toUpperCase()).split(",",1));
+          if(isFrequencia != -1) {
+            console.log('Amanhã terá coleta SELETIVA. Das ' + data.horario_inicio + ' às ' + data.horario_termino)
+            client
+            .sendText(message.from, 'Amanhã terá coleta SELETIVA. Das ' + data.horario_inicio + ' às ' + data.horario_termino)
+            .then((result) => {
+              console.log('Result: ', result); //return object success
+            })
+            .catch((erro) => {
+              console.error('Error when sending: ', erro); //return object error
+            });
+          } else {
+            console.log('Amanhã não tem coleta SELETIVA');
+          }
+        });
+      });
+
+    }
+  });
+}
