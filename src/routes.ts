604ab0dc4c3dc647c232f80f209aa49e18e61f52
Jallapeno <hytalo48@gmail.com>
2022-12-09 22:42:08 -0300

    New routes configured

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,33 @@
 import { Router } from 'express';
 const routes = Router();
+import { CollectController, UserController } from './controllers'
+import { User } from './models';
 
-const coletaController = require('./controllers/coletaController');
+const userController = new UserController();
+// const collectController = new CollectController;
 
-routes.get('/', coletaController.index);
-routes.get('/coleta/:cep', coletaController.coleta);
+
+routes.get('/', async(req, res) => {
+    return await userController.index().then((response) => {
+        res.status(200).send(response)
+    }).catch((err) => {
+        return res.status(res.statusCode || 400).json({
+            message: err.message || 'Unexpected error.'
+        })
+    })
+});
+
+routes.post('/users/register', async (req, res) => {
+    const body: User = req.body;
+    return await userController.register(body).then((response) => {
+        res.status(201).send(response)
+    }).catch((err) => {
+        return res.status(res.statusCode || 400).json({
+            message: err.message || 'Unexpected error.'
+        })
+    })
+})
+
+// routes.get('/coleta/:cep', coletaController.coleta);
 
 export { routes };
